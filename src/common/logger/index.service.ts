0df8cce1d8a40f8b1ad073fdfa584d7024851637
chenk4 <chenk4@knownsec.com>
2019-07-17 18:12:00 +0800

    feat: project init

--- src/common/logger/index.service.ts
+++ src/common/logger/index.service.ts
@@ -3,6 +3,9 @@ import * as moment from 'moment';
 import * as path from 'path';
 import { createLogger, format, transports } from 'winston';
 import * as DailyRotateFile from 'winston-daily-rotate-file';
+/* import { ConfigService } from '../config/index.service';
+const configService = new ConfigService();
+console.log(configService.get('elk')); // 接elk*/
 
 const { combine, timestamp, printf } = format;
 
